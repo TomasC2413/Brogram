f2226697568c7636134c09211e6a2f55db51fedb
TomasC2413 <tc285904@my.stchas.edu>
2026-02-10 14:47:40 -0500

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,42 @@
+export default function Hero() {
+    return (
+        <>
+            <h5>Complete this training program if you want to ...</h5>
+            <ol className="benefits-list">
+                <li>Follow a simple program wit guaranteed results</li>
+                <li>Get fit, healthy, stron and shredded</li>
+                <li>Learn more about gym, training and technique</li>
+                <li>Become a lifetime gym bro 🔱</li>
+            </ol>
+            <h3>The Rules</h3>
+            <p>To complete this program, you <b>MUST</b> follow these
+            3 simple rules:</p>
+            <ul className="rules-list">
+                <div className="rule-item">
+                    <p><b>Rest</b></p>
+                    <p>Ensure that you are taking rest days where
+                    necessary</p>
+                </div>
+                <div className="rule-item">
+                    <p><b>Reps</b></p>
+                    <p>Every rep is a pause rep following a <abbr title="2 seconds down - 2 seconds pause - 2 seconds up">2 - 2 - 2 
+                    tempo</abbr></p>
+                </div>
+                <div className="rule-item">
+                    <p><b>Weight*</b></p>
+                    <p>Select the maximun weight that allows you to 
+                    complete the set with good form</p>
+                </div>
+            </ul>
+            <small>* The first and second set should be at 75% and 
+            85% of your working weight used for the last two sets.</small>
+            <h3>The training Plan</h3>
+            <p>This training plan uses a structure know as the <b>Bro 
+            Split</b>, and follows this rotation ⬇️</p>
+            <p><b><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i></b></p>
+            <p>Complete all of the workouts bellow and track your progress 
+            along the way ✅</p>
+        </>
+
+    )
+}
